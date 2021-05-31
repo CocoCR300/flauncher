3fe9b025e0ce2cdf6fd003be4216285e2950a17a
Étienne Fesser <etiennf01@gmail.com>
2021-05-31 18:32:55 +0200

    feat: add Firebase Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,41 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flauncher/apps.dart';
 import 'package:flauncher/flauncher.dart';
 import 'package:flauncher/flauncher_channel.dart';
 import 'package:flauncher/wallpaper.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  await FirebaseCrashlytics.instance
+      .setCrashlyticsCollectionEnabled(kReleaseMode);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  Isolate.current.addErrorListener(RawReceivePort((List<dynamic> pair) async {
+    final List<dynamic> errorAndStacktrace = pair;
+    await FirebaseCrashlytics.instance.recordError(
+      errorAndStacktrace.first,
+      errorAndStacktrace.last as StackTrace,
+    );
+  }).sendPort);
+
+  runZonedGuarded<void>(() {
+    runApp(App());
+  }, FirebaseCrashlytics.instance.recordError);
 }
 
 class App extends StatelessWidget {
